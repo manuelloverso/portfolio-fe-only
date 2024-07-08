03edae7e8896e8655d37298ced1e1cf7ce81d34d
Manuel <manuelloverso60@gmail.com>
2024-07-08 16:46:23 +0200

    add footer structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,14 +8,37 @@ export default {
 </script>
 <template>
   <footer id="app-footer">
-    <div class="md-container">
-      <h3>I am The footer</h3>
+    <div class="md-container row">
+      <div class="col-12 col-md-4">
+        <nav class="d-flex flex-column g-3">
+          <div class="logo">ML</div>
+
+          <span>Progetti</span>
+          <span>Contattami</span>
+        </nav>
+      </div>
+
+      <div class="col-12 col-md-4">
+        <div class="socials d-flex g-3">
+          <span>Linkedin</span>
+          <span>Github</span>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4">
+        <div class="portfolio-links d-flex flex-column g-3">
+          <h3>Codice del Portfolio</h3>
+          <span>Front-end Repo</span>
+          <span>Back-end Repo</span>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
 <style scoped>
 #app-footer {
-  height: 400px;
+  padding: 3rem 0;
   background-color: rgba(10, 10, 10, 0.545);
+  color: rgb(197, 205, 209);
 }
 </style>
