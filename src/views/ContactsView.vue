04ddc54f3244b1ae153f4508bc5cbe4248701831
Manuel <manuelloverso60@gmail.com>
2024-07-19 12:43:17 +0200

    remove font awesome cdn

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -137,7 +137,16 @@ export default {
       <div class="mail">
         <h4>
           <a target="_blank" href="mailto:manuel.loverso.dev@gmail.com">
-            <i class="fa-regular fa-envelope"></i> manuel.loverso.dev@gmail.com
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              class="icon"
+            >
+              <path
+                d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z"
+              />
+            </svg>
+            manuel.loverso.dev@gmail.com
           </a>
         </h4>
         <span>Oppure contattami tramite il form qui sotto</span>
@@ -230,6 +239,13 @@ export default {
   & h4 {
     font-size: 5vw;
     margin-bottom: 20px;
+
+    & a {
+      .icon {
+        width: 20px;
+        fill: var(--accent);
+      }
+    }
   }
 
   & span {
